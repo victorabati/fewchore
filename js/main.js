d09aa55be5eea863342e918e924509cca4efb64c
Victor Newt <35165700+victorabati@users.noreply.github.com>
2022-02-14 16:42:16 +0100

    initializing

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(function() {
+    $('.scroll-down').click (function() {
+      $('html, body').animate({scrollTop: $('section.wwdSection').offset().top }, 'slow');
+      return false;
+    });
+  });
+
+/* Initialize Swiper */
+var swiper = new Swiper('.wwdSwiper', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2,
+        slideShadows: true,
+      },
+    loop: true
+    });
+
+/* FAQ Script */
+let li = document.querySelectorAll(".faq-text li");
+    for (var i = 0; i < li.length; i++) {
+      li[i].addEventListener("click", (e)=>{
+        let clickedLi;
+        if(e.target.classList.contains("question-arrow")){
+          clickedLi = e.target.parentElement;
+        }else{
+          clickedLi = e.target.parentElement.parentElement;
+        }
+       clickedLi.classList.toggle("showAnswer");
+      });
+    }
+
+    /* WOW */
+    $(function(){
+      new WOW().init(); 
+    });
+    
+    //also at the window load event
+    $(window).on('load', function(){
+    
+         new WOW().init(); 
+    });
